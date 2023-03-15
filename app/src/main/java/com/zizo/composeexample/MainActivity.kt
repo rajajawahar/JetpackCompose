4b8df8633f14d2f0b6d9c9ccb69f49f786e6a5a9
Raja Jawahar <rajajawahar77@gmail.com>
2023-03-15 07:57:36 +0530

    Modifiers added for Greeting Text

--- app/src/main/java/com/zizo/composeexample/MainActivity.kt
+++ app/src/main/java/com/zizo/composeexample/MainActivity.kt
@@ -3,9 +3,20 @@ package com.zizo.composeexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.zizo.composeexample.ui.theme.ComposeTheme
 
@@ -14,7 +25,6 @@ class MainActivity : ComponentActivity() {
     super.onCreate(savedInstanceState)
     setContent {
       ComposeTheme {
-        // A surface container using the 'background' color from the theme
         Greeting(name = "Raja Mohamed")
       }
     }
@@ -25,7 +35,17 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String) {
   Text(
     text = "Hello $name!",
-    fontSize = 30.sp
+    fontSize = 30.sp,
+    fontWeight = FontWeight.Bold,
+    textAlign = TextAlign.Center,
+    modifier = Modifier
+      .background(color = Color.Yellow)
+      .border(5.dp, color = Color.Black)
+      .alpha(0.5f)
+      .padding(10.dp)
+      .fillMaxHeight()
+      .fillMaxWidth()
+
   )
 }
 
